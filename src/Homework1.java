90af0bca85e215f8134ed59852fd730a84a31d4f
SLimZik <frog.mst.die@gmail.com>
2017-03-15 00:06:16 +0300

    Задание 1

---
+++ src/Homework1.java
@@ -0,0 +1,12 @@
+
+public class Homework1 {
+    public static void main (String [] args){
+        int q = 20;
+        int w = 7;
+        int a = q/w;
+        int b = q%w;
+
+        System.out.println("20/3 = " + a + " и " + b + " в остатке");
+    }
+
+}
