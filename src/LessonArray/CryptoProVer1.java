dd56dd833c668fc318f2b93d6b7eb8b5f070077d
SLimZik <frog.mst.die@gmail.com>
2017-03-19 18:00:35 +0300

    Задачи

---
+++ src/LessonArray/CryptoProVer1.java
@@ -0,0 +1,26 @@
+package LessonArray;
+
+/* ещё работа с ^ XOR */
+public class CryptoProVer1 {
+    public static void main(String[] args) {
+        String text = "После прочтения сжечь";
+
+        byte key = 50;
+
+        byte[] bytes = text.getBytes();
+
+        crypt(bytes, key);
+
+        System.out.println("Зашифрованное сообщение - " + new String(bytes));
+
+        crypt(bytes, key);
+
+        System.out.println("Дешифровка - " + new String(bytes));
+    }
+
+    private static void crypt(byte[] bytes, byte key) {
+        for (int i = 0; i < bytes.length; i++) {
+            bytes[i] ^= key;
+        }
+    }
+}
